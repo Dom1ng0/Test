2530930451459a3aabe01d41a03726002084aba6
Dom1ng0 <mvv7786@gmail.com>
2017-03-27 18:17:38 +0300

    Refactor all methods

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -21,15 +21,34 @@ public class RegistrationTest {
 
     @Test
     public void testRegistrationNewUser() throws Exception{
-        driver.findElement(By.cssSelector("input[data-qa='txtName']")).sendKeys("tester"); //name
-        driver.findElement(By.cssSelector("input[data-qa='txtEmail']")).sendKeys("test" + Math.random() + "@test.com");
-        driver.findElement(By.cssSelector("input[data-qa='txtPassword']")).sendKeys("test123"); //pass
+        typeUserName();
+        typeEmail();
+        typePassword();
+        selectDropdown("Student (Non-native Speaker)");
+        clickRegistrationButton();
 
-        Select dropdown = new Select(driver.findElement(By.cssSelector("select[data-qa='txtRole']")));
-        dropdown.selectByVisibleText("Student (Non-native Speaker)");
-
-        driver.findElement(By.cssSelector("input[data-qa='btnSignUp']")).click();
         AssertEquals("Hello, tester", driver.findElement(By.cssSelector("span[data-qa='txtUsername']")).getText());
+    }
+
+    private void typePassword() {
+        driver.findElement(By.cssSelector("input[data-qa='txtPassword']")).sendKeys("test123"); //pass
+    }
+
+    private void typeEmail() {
+        driver.findElement(By.cssSelector("input[data-qa='txtEmail']")).sendKeys("test" + Math.random() + "@test.com");
+    }
+
+    private void typeUserName() {
+        driver.findElement(By.cssSelector("input[data-qa='txtName']")).sendKeys("tester"); //name
+    }
+
+    private void clickRegistrationButton() {
+        driver.findElement(By.cssSelector("input[data-qa='btnSignUp']")).click();
+    }
+
+    private void selectDropdown(String value) {
+        Select dropdown = new Select(driver.findElement(By.cssSelector("select[data-qa='txtRole']")));
+        dropdown.selectByVisibleText(value);
     }
 
     @After
