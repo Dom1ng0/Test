ece3de43abd563ed73855718bf1b1a221bc31bd8
Dom1ng0 <mvv7786@gmail.com>
2017-03-28 14:23:19 +0300

    BeforeClass/AfterClass added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,22 +5,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    protected WebDriver driver;
+    protected static WebDriver driver;
     protected void AssertEquals(String s, String text){}
     protected String baseUrl = "https://www.grammarly.com/";
     protected String loginUrl = baseUrl + "signin?page=free";
     protected String registrationUrl = baseUrl + "signup?breadcrumbs=true&no_install=true";
 
-    @Before
-    public void setUp() throws Exception{
+    @BeforeClass
+    public static void beforeAll(){
         ChromeDriverManager.getInstance().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        System.out.println("BEFORE CLASS IS EXECUTED");
+    }
+    @Before
+    public void setUp() throws Exception{
+        System.out.println("BEFORE TEST IS EXECUTED");
     }
     @After
     public void tearDown() throws Exception{
+        driver.manage().deleteAllCookies();
+        System.out.println("AFTER TEST IS EXECUTED");
+    }
+    @AfterClass
+    public static void afterAll() {
         driver.quit();
+        System.out.println("AFTER CLASS IS EXECUTED");
     }
 
 }
