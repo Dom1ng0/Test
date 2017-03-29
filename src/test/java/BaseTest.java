b9b9f1d6f9d703e7f512ce3bfd8090a894886132
Dom1ng0 <mvv7786@gmail.com>
2017-03-29 14:44:51 +0300

    Import changes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,8 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
