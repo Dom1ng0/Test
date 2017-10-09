bfee6d2e7d697e81f660cff5692f8a8eb01d352a
dom1ng0 <mvv7786@gmail.com>
2017-10-09 14:27:06 +0300

    Some changes in registration method according to new design

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -14,35 +14,28 @@ public class RegistrationTest extends BaseTest {
     @Test
     public void testRegistrationNewUser() throws Exception {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registration("tester", "test" + Math.random() + "@test.com", "test123", "Other");
-        AssertEquals("Hello, tester", driver.findElement(By.cssSelector("span[data-qa='txtUsername']")).getText());
+        registrationPage.registration("test" + Math.random() + "@test.com", "test123", "tester");
+        AssertEquals("Hello, tester", driver.findElement(By.cssSelector("[data-qa='txtUsername']")).getText());
     }
 
     @Test
     public void testRegistrationNoUserName() throws Exception {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registration("", "test" + Math.random() + "@test.com", "test123", "Other");
-        AssertEquals("Required", driver.findElement(By.cssSelector("div[data-qa='msgErrorName']")).getText());
+        registrationPage.registration("test" + Math.random() + "@test.com", "test123", "");
+        AssertEquals("Required", driver.findElement(By.cssSelector("[data-qa='errName']")).getText());
     }
 
     @Test
     public void testRegistrationNoEmail() throws Exception {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registration("tester", "", "test123", "Other");
-        AssertEquals("Required", driver.findElement(By.cssSelector("div[data-qa='msgErrorEmail']")).getText());
+        registrationPage.registration("", "test123", "tester");
+        AssertEquals("Required", driver.findElement(By.cssSelector("[data-qa='errEmail']")).getText());
     }
 
     @Test
     public void testRegistrationNoPassword() throws Exception {
         RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registration("tester", "test" + Math.random() + "@test.com", "", "Other");
-        AssertEquals("Required", driver.findElement(By.cssSelector("div[data-qa='msgErrorPassword']")).getText());
-    }
-
-    @Test
-    public void testRegistrationNotSelectedDropdown() throws Exception {
-        RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.registration("tester", "test" + Math.random() + "@test.com", "test123", "");
-        AssertEquals("Required", driver.findElement(By.cssSelector("div[data-qa='msgErrorundefined']")).getText());
+        registrationPage.registration("test" + Math.random() + "@test.com", "", "tester");
+        AssertEquals("Required", driver.findElement(By.cssSelector("[data-qa='errPassword']")).getText());
     }
 }
